21924ae87e23353c9dca52cc7cc924233e33b5d4
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-05-11 21:37:40 +0100

    style up components

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     return (
         <ApolloProvider client={client}>
             <div id="main">
-                <h1>Ninja's Reading List</h1>
+                <h1>Graph Books</h1>
                 <BookList />
                 <AddBook />
             </div>
